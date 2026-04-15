0bfae52c260f97e7ce4b8c0f847ed0b003ade048
Joaquin <joakinpro100@gmail.com>
2026-04-14 23:58:33 -0300

    Cambio

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola como estan!</Text>
+      <Text>Hola como estannnn!</Text>
       <StatusBar style="auto" />
     </View>
   );
