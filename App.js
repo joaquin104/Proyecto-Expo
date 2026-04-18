aa1dbfae3f6aa9bcfb49cccb69ca39476ccad9f3
Joaquin <joakinpro100@gmail.com>
2026-04-18 17:03:38 -0300

    Errores

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
+import { Ejercicio4 } from './components/Ejercicio4';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+       <Ejercicio4></Ejercicio4>
       <Text>Hola como estannnn!</Text>
       <Text>Probando!</Text>
       <StatusBar style="auto" />
---
+++ components/Ejercicio4.js
@@ -0,0 +1,22 @@
+import { Text, View, TextInput, StyleSheet} from 'react-native';
+
+
+export const Ejercicio4 = () => {
+  return (
+    <View style={styles.container}>
+<Text>Hola como estan!!!!!!!!asdad</Text>
+</View>
+  )}
+
+const styles = StyleSheet.create({
+
+container: {
+  flex:1,
+  justifyContent: 'center',
+  alignItems:'center',
+}
+
+
+
+})
+
